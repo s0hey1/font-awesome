afbcc8da1510ea4cf46823f3a79f2d5a72252267
shu zOMG chen <shu.chen@freelancedreams.com>
2012-02-16 23:11:18 -1000

    freeing memory, graceful failure flag

--- src/font-awesome.c
+++ src/font-awesome.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
 #include <wchar.h>
 #include <locale.h>
 #include <png.h>
@@ -17,7 +16,13 @@
 #define PNG_PIXELDEPTH 8
 #define FONTAWESOME_VERSION_MAJOR 1
 #define FONTAWESOME_VERSION_MINOR 0
-#define FONTAWESOME_VERSION_PATCH 0
+#define FONTAWESOME_VERSION_PATCH 1
+
+//Flags
+#define FONTAWESOME_FLAG_HASERROR      0x1
+#define FONTAWESOME_FLAG_EMPTY         0x2
+#define FONTAWESOME_FLAG_VERBOSE       0x4
+#define FONTAWESOME_FLAG_GRACEFULEMPTY 0x8
 
 typedef struct{
 	uint8_t r;
@@ -80,7 +85,6 @@ int main(int argc, char** argv){
 
 	char *argv_filename = NULL;
 	int argv_fontsize = 32;
-	bool argv_verbose = false;
 
 	wchar_t text[TEXT_LENGTH_LIMIT];
 	wchar_t c;
@@ -90,9 +94,9 @@ int main(int argc, char** argv){
 	int minx, miny, maxx, maxy;
 	int width, height;
 	int offset;
+	int flags = 0;
 
 	FT_Library library;
-	FT_Error error;
 	FT_Face face;
 	FT_Vector pen;
 	FT_GlyphSlot slot;
@@ -112,6 +116,9 @@ int main(int argc, char** argv){
 				return show_version();
 				break;
 			}
+			if ((strcmp(argv[i], "--gracefulempty"))==0){
+				flags |= FONTAWESOME_FLAG_GRACEFULEMPTY;
+			}
 			switch (argv[i][1]){
 				case 'h':
 					return show_help();
@@ -123,7 +130,7 @@ int main(int argc, char** argv){
 					argv_fontsize = atoi(argv[++i]);
 					break;
 				case 'v':
-					argv_verbose = true;
+					flags |= FONTAWESOME_FLAG_VERBOSE;
 					break;
 			}
 		}
@@ -134,14 +141,12 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
-	freopen(NULL, "rb", stdin);
-	i = 0;
-
-	if (argv_verbose){
+	if (flags & FONTAWESOME_FLAG_VERBOSE){
 		fprintf(stderr, "wchar_t is %zu\n", sizeof(wchar_t));
 		fprintf(stderr, "stdin codepoints are as follows:\n");
 	}
 
+	freopen(NULL, "rb", stdin);
 	while(1){
 		c = fgetwc(stdin);
 		if (c == 10){ /* line feed */
@@ -151,25 +156,29 @@ int main(int argc, char** argv){
 			break;
 		}
 		text[text_length] = c;
-		if (argv_verbose){
+		if (flags & FONTAWESOME_FLAG_VERBOSE){
 			fprintf(stderr, "%d: 0x%X (%lc)\n", text_length, (wint_t)c, (wint_t)c);
 		}
 		text_length++;
 	}
 
-	error = FT_Init_FreeType(&library);
-	if (error){
+	if (FT_Init_FreeType(&library)){
 		fprintf(stderr, "error initializing freetype\n");
-		return 1;
+		flags |= FONTAWESOME_FLAG_HASERROR;
+		goto GOTO_ERROR_INIT_FREETYPE;
 	}
 
-	error = FT_New_Face(library, argv_filename, 0, &face);
-	if (error){
+	if (FT_New_Face(library, argv_filename, 0, &face)){
 		fprintf(stderr, "error loading font\n");
-		return 1;
+		flags |= FONTAWESOME_FLAG_HASERROR;
+		goto GOTO_ERROR_NEW_FACE;
 	}
 
-	error = FT_Set_Char_Size(face, 0, argv_fontsize * 64, FREETYPE_DPI, FREETYPE_DPI);
+	if (FT_Set_Char_Size(face, 0, argv_fontsize * 64, FREETYPE_DPI, FREETYPE_DPI)){
+		fprintf(stderr, "error setting char_size\n");
+		flags |= FONTAWESOME_FLAG_HASERROR;
+		goto GOTO_ERROR_SET_CHAR_SIZE;
+	}
 	slot = face->glyph;
 
 	pen.x = 0;
@@ -195,26 +204,32 @@ int main(int argc, char** argv){
 	width = maxx - minx;
 	height = maxy - miny;
 
+	if ((width == 0) | (height == 0)){
+		flags |= FONTAWESOME_FLAG_HASERROR;
+		flags |= FONTAWESOME_FLAG_EMPTY;
+
+		if (flags | FONTAWESOME_FLAG_GRACEFULEMPTY){
+			if (flags | FONTAWESOME_FLAG_VERBOSE){
+				fprintf(stderr, "Empty image, returning 1x1 empty png\n");
+				goto GOTO_ERROR_FT;
+			}
+		}else{
+			goto GOTO_ERROR_FT;
+		}
+	}
+
 	pen.x = (0 - minx) * 64;
 	pen.y = maxy * 64;
 
-	image.width = width;
-	image.height = height;
-	image.pixels = calloc (sizeof (pixel_t), width * height);
-
-	for (i = 0; i < text_length; i++){
-		FT_Set_Transform(face, NULL, &pen);
-		FT_Load_Char(face, text[i], FT_LOAD_RENDER);
-		draw_bitmap(&image, &slot->bitmap, slot->bitmap_left, height - slot->bitmap_top);
-		pen.x += slot->advance.x;
-		pen.y += slot->advance.y;
-	}
+	image.width = (flags | FONTAWESOME_FLAG_EMPTY) ? 1: width;
+	image.height = (flags | FONTAWESOME_FLAG_EMPTY) ? 1 :  height;
+	image.pixels = calloc (sizeof(pixel_t), image.width * image.height);
 
 	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	info_ptr = png_create_info_struct(png_ptr);
 	png_init_io(png_ptr, stdout);
 
-	png_set_IHDR(png_ptr, info_ptr, width, height,
+	png_set_IHDR(png_ptr, info_ptr, image.width, image.height,
 		PNG_PIXELDEPTH, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE,
 		PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
@@ -225,23 +240,46 @@ int main(int argc, char** argv){
 	png_write_info(png_ptr, info_ptr);
 	png_set_packing(png_ptr);
 
-	for (i=0; i<height; i++){
-		row = (png_bytep) malloc(sizeof(png_byte) * PNG_PIXELSIZE * width);
-		for (j=0; j<width; j++){
-			offset = j * PNG_PIXELSIZE;
-			pixel = pixel_at(&image, j, i);
-			row[offset] = pixel->r;
-			row[offset + 1] = pixel->g;
-			row[offset + 2] = pixel->b;
-			row[offset + 3] = pixel->a;
-		}
+	row = (png_bytep) malloc(sizeof(png_byte) * PNG_PIXELSIZE * image.width);
+	if (flags | FONTAWESOME_FLAG_EMPTY){
+		memset(row, 0, sizeof(row));
 		png_write_row(png_ptr, row);
+	}else{
+		for (i = 0; i < text_length; i++){
+			FT_Set_Transform(face, NULL, &pen);
+			FT_Load_Char(face, text[i], FT_LOAD_RENDER);
+			draw_bitmap(&image, &slot->bitmap, slot->bitmap_left, height - slot->bitmap_top);
+			pen.x += slot->advance.x;
+			pen.y += slot->advance.y;
+		}
+
+		for (i=0; i<height; i++){
+			memset(row, 0, sizeof(row));
+			for (j=0; j<width; j++){
+				offset = j * PNG_PIXELSIZE;
+				pixel = pixel_at(&image, j, i);
+				row[offset] = pixel->r;
+				row[offset + 1] = pixel->g;
+				row[offset + 2] = pixel->b;
+				row[offset + 3] = pixel->a;
+			}
+			png_write_row(png_ptr, row);
+		}
 	}
+	if (row != NULL) free(row);
+
 	png_write_end(png_ptr, NULL);
 
 	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
 	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-	if (row != NULL) free(row);
-	return 0;
+
+	GOTO_ERROR_FT:
+	GOTO_ERROR_SET_CHAR_SIZE:
+	FT_Done_Face(face);
+	GOTO_ERROR_NEW_FACE:
+	FT_Done_FreeType(library);
+	GOTO_ERROR_INIT_FREETYPE:
+
+	return (flags & FONTAWESOME_FLAG_HASERROR) ? 1 : 0;
 }
 
