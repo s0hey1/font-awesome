677de36a6d1f242abef4ef6b2dc84ea22135e847
shu zOMG chen <shu.chen@freelancedreams.com>
2013-04-16 10:05:04 -1000

    Test text-color in test.php

--- test.php
+++ test.php
@@ -28,7 +28,7 @@ function utf8_to_unicode($str) {
 
 mb_internal_encoding("UTF-8");
 $pipes = array();
-$cmd = "./font-awesome -v --as-codepoints -f testfonts/1.ttf >output.png";
+$cmd = "./font-awesome -v --text-color f0f000FF --as-codepoints -f testfonts/1.ttf >output.png";
 $desc = array(
 	array("pipe", "rb"),
 	array("pipe", "wb"),
