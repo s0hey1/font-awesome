18ad79d6d83719f2ed9063559c14130d6b8b89c9
Josh Farr <josh@creativemarket.com>
2013-08-13 17:15:49 -0700

    fix rendering.

--- src/Renderer.cxx
+++ src/Renderer.cxx
@@ -31,12 +31,17 @@ boost::shared_ptr<Image> Renderer::render(const Font & font, const Color & color
 	pen.second = range.max_.second * penDPI;
 
 	size_t length = text.length();
+	if (debug_) {
+		std::cout << "rendering [" << length << "] characters to image [" << image->width() << "x" << image->height() << "]" << std::endl;
+	}
 	for (size_t index = 0; index < length; ++index) {
 		Font::Glyph glyph = font.glyph(text[index], pen);
 
 		// if glyph is empty & fix missing is enabled then draw empty glyph
 		if (glyph.empty_) {
-
+			if (debug_) {
+				std::cout << "empty glyph [" << text[index] << "] at index [" << index << "]" << std::endl;
+			}
 		}
 		else {
 			blit(image, glyph, color);
@@ -51,18 +56,21 @@ boost::shared_ptr<Image> Renderer::render(const Font & font, const Color & color
 void Renderer::blit(const boost::shared_ptr<Image> & image, const Font::Glyph & glyph, const Color & color) {
 	size_t width  		= glyph.size_.first;
 	size_t height 		= glyph.size_.second;
-	size_t x 	  		= glyph.position_.first;
 	size_t canvasWidth  = image->width();
 	size_t canvasHeight = image->height();
+	size_t x 	  		= glyph.position_.first;
 	size_t y 			= canvasHeight - glyph.position_.second;
 	const size_t bpp 	= 4;
 	size_t pixel;
+	size_t flipY;
+	if (debug_) {
+		std::cout << "glyph size [" << width << "x" << height << "] @ [" << x << "x" << y << "]" << std::endl;
+	}
 	for (size_t i = 0; i < width; i++) {
 		for (size_t j = 0; j < height; j++) {
 			if (glyph.bitmap_[j * width + i] != 0) {
-				// x = x + i; y = y + j
-				// ((y + j) * width * 4) + ((x + i) * 4);
-				pixel = ((y + j) * width * bpp) + ((x + i) * bpp);
+				flipY = height - j - 1 - y; // freetype rows are backwards so flip them around
+				pixel = ((flipY + y) * canvasWidth * bpp) + ((x + i) * bpp);
 				(*image)[pixel] 	 = color.red();
 				(*image)[pixel + 1]  = color.green();
 				(*image)[pixel + 2]  = color.blue();
